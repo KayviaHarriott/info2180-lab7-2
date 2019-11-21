6e1448534a19cc76c8e56c4fdb7bea5c8a0008bb
Ubuntu <ubuntu@ip-172-31-33-114.ec2.internal>
2019-11-21 02:07:39 +0000

    Updated world.php

--- world.php
+++ world.php
@@ -4,14 +4,41 @@ $username = 'kayvia123';
 $password = 'Harriott123!';
 $dbname = 'world';
 
+
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4",$username,$password);
-$stmt = $conn->query("SELECT * FROM countries");
+//$stmt = $conn->query("SELECT * FROM countries");
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
+
+<form method="GET">
+<input name="country">
+<input type="submit" value="SEARCH">
+</form>
+
+<?php echo $_GET['country'];?>
+<table>
+    <tr>
+        <th>Country Name</th>
+        <th>Continent</th>
+        <th>Independence Year</th>
+        <th>Head of State</th>
+    </tr>
+    <tr>
+        
+    </tr>
+
+
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+    <tr>
+       <td><?= $row['name']; ?></td>
+       <td><?= $row['continent']; ?></td>
+       <td><?= $row['independence_year']; ?></td>
+       <td><?= $row['head_of_state']; ?></td>
+    </tr>
+
+<?php endforeach; ?> 
+<table> 
